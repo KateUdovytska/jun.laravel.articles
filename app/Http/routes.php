c2bbe25a8f2449ffa6e642a107aa6c5b9342c9d4
kate <ketrina113@gmail.com>
2020-12-16 15:41:28 +0200

    routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,71 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Article;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'articles'], function () {
+    /**
+     *Вывести панель статей
+     */
+    Route::get('/', function () {
+        $articles = Article::orderBy('created_at', 'asc')->get();
+
+        return view('articles.index', [
+            'articles' => $articles
+        ]);
+
+    })->name('articles.all');
+
+    /**
+     *Добавить новую статью
+     */
+    Route::post('/', function (Request $request) {
+        $validator = Validator::make($request->all(),
+            [
+                'title' => 'required|max:255',
+                'description' => 'required'
+            ]);
+        if ($validator->fails()) {
+            return redirect(route('articles.all'))
+                ->withInput()
+                ->withErrors($validator); //для вывода ошибок
+        }
+        $title = $request->title;
+        $description = $request->description;
+        //через create ask =========
+//        Article::create([['title'=>$title],['description'=>$description]]);
+        //========================
+        $article = new Article();
+        $article->title = $title;
+        $article->description = $description;
+        $article->save();
+        return redirect(route('articles.all'));
+    })->name('article.add');
+
+    /**
+     *Удалить статью
+     */
+    Route::delete('/{article}', function (Article $article) {
+        $article->delete();
+        return redirect(route('articles.all'));
+    })->name('article.delete');
+
+    /**
+     *Вывести текст статьи
+     */
+    Route::get('/{article}', function (Article $article) {
+//        $articles = Article::orderBy('created_at', 'asc')->get(); //TODO
+            //$articles = Article::get();
+        $articles = [
+            'title' =>  $article->title,
+            'description'=>$article->description,
+        ];
+
+
+
+        return view('article.article', [
+            'articles' => $articles
+        ]);
+    })->name('article.show');
 });
+
